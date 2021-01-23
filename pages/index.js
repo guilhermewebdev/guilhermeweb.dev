4d137675c818faae5278a306cdadb0706a1da6a2
Guilherme Isaias <guilherme@guilhermeweb.dev>
2021-01-22 21:13:07 -0300

    Criação da seção sobre

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,45 @@ const menu = [
 ]
 const name = 'Guilherme Isaías'
 const slogan = 'Transformo ideias em software, ajudando empresas a automatizarem e melhorarem processos'
-
+const about = `Olá, sou Guilherme Isaías, desenvolvedor de software Full-Stack e Full-Cycle. 
+              Especialista no desenvolvimento de sistemas web, e possuo experiência com a 
+              criação de plataformas, lojas virtuais, automação comercial, entre outros. 
+              Sempre utilizando as melhores práticas de desenvolvimento, com testes automatizados,
+              Clean Code, integração contínua, entre outros, facilitando a manutenção, escalabilidade 
+              e garantindo a estabilidade dos sistemas.
+            `
+const occupation = 'Desenvolvedor Web Autônomo'
+const quote = 'As grandes conquistas da humanidade foram obtidas conversando, e as grandes falhas pela falta de diálogo.'
+const quoteAuthor = 'Stephen Hawking'
+const skills = [
+  {
+    name: 'Python',
+  },
+  {
+    name: 'Django'
+  },
+  {
+    name: 'Docker'
+  },
+  {
+    name: 'JavaScript / Typescript'
+  },
+  {
+    name: 'React.js / React Native'
+  },
+  {
+    name: 'Node.js'
+  },
+  {
+    name: 'DevOps / FullCycle'
+  },
+  {
+    name: 'Computação em núvem'
+  },
+  {
+    name: 'Linux',
+  }
+]
 export default function Home() {
   return (
     <>
@@ -82,7 +120,29 @@ export default function Home() {
         </nav>
       </header>
       <main>
-
+        <section id="about">
+          <h3>Sobre</h3>
+          <p>{about}</p>
+          <section>
+            <h5>Ocupação atual</h5>
+            <p>{occupation}</p>
+          </section>
+          <section>
+            <h5>Citação</h5>
+            <blockquote>{quote}</blockquote>
+            <cite>{quoteAuthor}</cite>
+          </section>
+          <section>
+            <h5>Habilidades</h5>
+            <ul>
+              {skills.map(skill => (
+                <li>
+                  <span>{skill.name}</span>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </section>
       </main>
     </>
   )
