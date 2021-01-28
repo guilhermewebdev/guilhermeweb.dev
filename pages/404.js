abc23ff2b58d5fb962bb05caec5c6cacb0594447
Guilherme Isaias <guilherme@guilhermeweb.dev>
2021-01-27 23:44:00 -0300

    Página de erro 404

---
+++ pages/404.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import styles from '../styles/404.module.scss'
+import Link from 'next/link'
+import home from '../styles/Home.module.scss'
+
+
+function createLine(amount) {
+    return new Array(amount).fill(0).map(() => Math.round(Math.random() * (0 + 1))).join('');
+}
+function getBackgroundMatrix() {
+    if (typeof window !== 'undefined') {
+        const height = Math.round(window.innerHeight / 30);
+        const width = Math.round(window.innerWidth / 15);
+        const matrix = new Array(height).fill(0).map(() => createLine(width))
+        return matrix
+    }
+    return []
+}
+export default function Custom404() {
+    const background = getBackgroundMatrix()
+    return (
+        <main className={styles.main}>
+            <section className={styles.section}>
+                <h1>404</h1>
+                <p>Página não encontrada</p>
+                <Link href="/">
+                    <a className={home.button}>Voltar ao Início</a>
+                </Link>
+                <div className={styles.bgText}></div>
+            </section>
+            <div className={styles.background}>
+                {background.map((row, rowIndex) => (
+                    <span key={rowIndex}>
+                        {row}
+                    </span>
+                ))}
+            </div>
+        </main>
+    )
+}
